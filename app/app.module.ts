8611ec44084a8f880a8688c2834b96947695f053
Nolan Chinn <nolan694@gmail.com>
2016-11-30 17:34:29 -0800

    add some rc5 structure to the app

---
+++ app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent }  from './app.component';
+import { RequestService } from './requests.service';
+
+@NgModule({
+  imports:		[ BrowserModule ],   // module dependencies
+  declarations: [ AppComponent ],    // components and directives
+  bootstrap:	[ AppComponent ],    // root component
+  providers:	[ RequestService ]   // services
+})
+
+export class AppModule { }
--- aswwu-requests.ts
+++ aswwu-requests.ts
@@ -1,2 +1,2 @@
 
-export * from './src/requests.service';
+export * from './app/requests.service';
