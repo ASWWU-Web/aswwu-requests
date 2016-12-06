88fc08fd98beb3b37130f74266cda0b333683676
Nolan Chinn <nolan694@gmail.com>
2016-12-05 20:14:02 -0800

    add index.html for testing the service
     - TODO: get 404 resources figured out in index.html

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'my-app',
-	template: '<h1>Hello World</h1>'
+	template: '<h1>hello from ASWWU requests</h1>'
 })
 
 export class AppComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
-import { RequestService } from './requests.service';
+//import { RequestService } from './requests.service';
 
 @NgModule({
   imports:		[ BrowserModule ],   // module dependencies
   declarations: [ AppComponent ],    // components and directives
-  bootstrap:	[ AppComponent ],    // root component
-  providers:	[ RequestService ]   // services
+  bootstrap:	[ AppComponent ]    // root component
+  //providers:	[ ]   // services
 })
 
 export class AppModule { }
